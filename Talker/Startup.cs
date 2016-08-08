ea6edbbbd034b9cc7d961c6fc4fd0cdd3dab61c7
Juan Granda <juanfromecuador@gmail.com>
2016-08-08 10:04:56 -0400

    Add project files.

---
+++ Talker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Talker.Startup))]
+namespace Talker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
